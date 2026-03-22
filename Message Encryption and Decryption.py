14d7fcc7115c59e82d7269758fd064e9161d751c
Mayank L Surana <mayanksuranacoding1@gmail.com>
2026-03-22 10:31:29 +0530

    Add files via upload

---
+++ Message Encryption and Decryption.py
@@ -0,0 +1,34 @@
+import random
+import string
+
+chars = string.ascii_letters + string.punctuation + string.digits + " " 
+chars= list(chars)
+# print(chars)
+# print()
+keys = chars.copy()
+random.shuffle(keys)
+# print(keys)
+def encrption(plain_text):
+    cipher_text = ""
+    for letters in plain_text:
+        index = chars.index(letters)
+        cipher_text += keys[index]
+    return cipher_text
+
+def decryption(cipher_text):
+    plain_text = ""
+    for letters in cipher_text:
+        index = keys.index(letters)
+        plain_text += chars[index]
+    return plain_text
+
+#MESSAGE ENCRYPTION
+plain_text = input("Enter the message to encrypt: ")
+cipher_text = encrption(plain_text)
+print("Encrypted message: ", cipher_text)
+
+#MESSAGE DECRYPTION
+
+cipher_text = input("Enter the message to be Decrypted: ")
+plain_text = decryption(cipher_text)
+print("Decrypted message: ", plain_text)
