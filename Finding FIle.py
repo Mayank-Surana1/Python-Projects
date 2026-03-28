52a8b058b5a50e781727979b02b4f5ca238ad95d
Mayank L Surana <mayanksuranacoding1@gmail.com>
2026-03-28 23:47:09 +0530

    Enhance find_file function with additional comments
    Added comments to clarify file creation process.

--- Finding FIle.py
+++ Finding FIle.py
@@ -7,8 +7,8 @@ def find_file():
         print("File not found.")
         create = input("Do you want to create the file? (y/n): ")
         if create.lower() == 'y':
-            with open(file_path, 'w') as file:
-                print("File created successfully.")
+            with open(file_path, 'w') as file:    # "x" is used to create if does not exist
+                print("File created successfully.")  # "r" is used to extract the data from a file into a displayable outnput
         
 file_path = input("Enter the file path: ")     #Both relative and absolute path are acceptable
 find_file()
