26878b2e902e569f254fed84ecd23f454b7d61f8
Mayank L Surana <mayanksuranacoding1@gmail.com>
2026-03-30 11:56:32 +0530

    Refactor Stop Watch.py for clarity and consistency

--- Stop Watch.py
+++ Stop Watch.py
@@ -1,7 +1,7 @@
 import time
 
 print("press [ENTER] to start the stop watch")
-input()    #Checks for only the Enter key pressed, and ignores any other input
+input()   ##Checks for only the Enter key pressed, and ignores any other input
 starting_time = time.perf_counter()  #Performance Counter
 
 print("******************************")
@@ -20,10 +20,13 @@ Elapsed_time = ending_time - starting_time   #The time diffrence between the fir
 
 hours = Elapsed_time // 3600   #1 Hour = 3600 Seconds   this keeps the whole number part of the division, which represents the number of hours in the elapsed time. 
                                # The remaining seconds after calculating hours is used to calculate minutes and seconds.
-minutes = (Elapsed_time % 3600) // 60  #after calculating hours, the remaining seconds are calculated using the modulus operator (%), and then divided by 60 to get the number of minutes in the elapsed time.
+minutes = (Elapsed_time % 3600) // 60  #after calculating hours, the remaining seconds are calculated using the modulus operator (%),
+                                       # and then divided by 60 to get the number of minutes in the elapsed time.
 
 seconds = Elapsed_time % 60 #The remaining seconds after calculating hours and minutes is used to calculate seconds
 
 milliseconds = (Elapsed_time - int(Elapsed_time)) * 1000
 
 print(f"Elapsed time: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds, {int(milliseconds)} milliseconds")
+
+
